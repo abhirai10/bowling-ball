406ceecc525c7bf2b009160d7bbcb3eaf7c86b26
abhirai10 <abhi_rai10@rediffmail.com>
2021-07-01 20:22:05 +0530

    initial commit

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    /// <summary>
+    /// Test class where all the unit tests for Bowling Ball Program are written.
+    /// </summary>
+    [TestClass]
+    public class GameFixture
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            //arrange - Initialize a Bowling Ball Game
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            //act
+            Roll(game, 0, 20);
+
+            //assert
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Allones_game_score_should_be_twenty_test()
+        {
+            //act
+            Roll(game,1, 20);
+
+            //assert
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Spare_followed_by_two_game_score_should_be_fourteen_test()
+        {
+            //act
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(2);
+            Roll(game,0, 17);
+
+            //assert
+            Assert.AreEqual(14, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Strike_followed_by_five_then_two_game_score_should_be_twenty_four_test()
+        {
+            //act
+            game.Roll(10);
+            game.Roll(5);
+            game.Roll(2);
+            Roll(game,0, 17);
+
+            //assert
+            Assert.AreEqual(24, game.GetScore());
+        }
+
+        [TestMethod]
+        public void AllStrike_game_score_should_be_three_hundred_test()
+        {
+            //act
+            Roll(game,10, 12);
+
+            //assert
+            Assert.AreEqual(300, game.GetScore());
+        }
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    /// <summary>
+    /// A Bowling Ball Game.
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Field to store no. of pins down for every roll.
+        /// </summary>
+        private int[] rolls = new int[21];
+
+        /// <summary>
+        /// Current roll
+        /// </summary>
+        private int roll = 0;
+
+        /// <summary>
+        /// Store the number of pins down for the current roll.
+        /// </summary>
+        /// <param name="pins">Number of Pins down.</param>
+        public void Roll(int pins)
+        {
+            rolls[roll++] = pins;
+        }
+
+        /// <summary>
+        /// Get total Score of the Bowling Ball Game.
+        /// </summary>
+        /// <returns>Total Score of the game.</returns>
+        public int GetScore()
+        {
+            int score = 0;
+            int rollIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(rollIndex))
+                {
+                    score += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                    rollIndex++;
+                }
+                else if (IsSpare(rollIndex))
+                {
+                    score += 10 + rolls[rollIndex + 2];
+                    rollIndex += 2;
+                }
+                else
+                {
+                    score += rolls[rollIndex] + rolls[rollIndex + 1];
+                    rollIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        /// <summary>
+        /// Check if the roll was a Strike.
+        /// </summary>
+        /// <param name="rollIndex">Index of Roll.</param>
+        /// <returns>Returns true if Strike else false.</returns>
+        private bool IsStrike(int rollIndex)
+        {
+            return rolls[rollIndex] == 10;
+        }
+
+        /// <summary>
+        /// Check if the roll was a Spare.
+        /// </summary>
+        /// <param name="rollIndex">Index of Roll.</param>
+        /// <returns>Returns true if Spare else false.</returns>
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
+    }
+}
